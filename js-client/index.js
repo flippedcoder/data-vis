57a053736b9a2a43c3c7b7446ac3690865f3ac1d
flippedcoder <milecia.mcgregor@aol.com>
2021-11-08 14:14:07 -0500

    updated for course debugging

--- js-client/index.js
+++ js-client/index.js
@@ -1,4 +1,5 @@
-var data = [{
+var data = [
+    {
         "year": "2010",
         "amount": 134
     },
@@ -44,57 +45,62 @@ var data = [{
     }
 ]
 
-var svg = d3.select("#data-vis-container")
+fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(async (response) => {
+        fetchedData = await response.json()
+        var svg = d3.select("#data-vis-container")
 
-var margin = 200,
-    width = svg.attr("width") - margin,
-    height = svg.attr("height") - margin
+        var margin = 200,
+            width = svg.attr("width") - margin,
+            height = svg.attr("height") - margin
 
-svg.append("text")
-    .attr("transform", "translate(100,0)")
-    .attr("x", 50)
-    .attr("y", 50)
-    .attr("font-size", "24px")
-    .text("Example fluctuations")
+        svg.append("text")
+            .attr("transform", "translate(100,0)")
+            .attr("x", 50)
+            .attr("y", 50)
+            .attr("font-size", "24px")
+            .text("Example fluctuations")
 
-var xScale = d3.scaleBand().range([0, width]).padding(0.4),
-    yScale = d3.scaleLinear().range([height, 0]);
+        var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+            yScale = d3.scaleLinear().range([height, 0]);
 
-var g = svg.append("g")
-    .attr("transform", "translate(" + 100 + "," + 100 + ")");
+        var g = svg.append("g")
+            .attr("transform", "translate(" + 100 + "," + 100 + ")");
 
-xScale.domain(data.map(function(d) { return d.year; }));
-yScale.domain([0, d3.max(data, function(d) { return d.amount; })]);
+        xScale.domain(fetchedData.map(function (d) { return d.userId; }));
+        yScale.domain([0, d3.max(fetchedData, function (d) { return d.id; })]);
 
-g.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(xScale))
-    .append("text")
-    .attr("y", height - 250)
-    .attr("x", width - 100)
-    .attr("text-anchor", "end")
-    .attr("stroke", "black")
-    .text("Year");
+        g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale))
+            .append("text")
+            .attr("y", height - 250)
+            .attr("x", width - 100)
+            .attr("text-anchor", "end")
+            .attr("stroke", "black")
+            .text("Year");
 
-g.append("g")
-    .call(d3.axisLeft(yScale).tickFormat(function(d) {
-            return "*" + d;
-        })
-        .ticks(10))
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", "-5.1em")
-    .attr("text-anchor", "end")
-    .attr("stroke", "black")
-    .text("Amount");
+        g.append("g")
+            .call(d3.axisLeft(yScale).tickFormat(function (d) {
+                return "*" + d;
+            })
+                .ticks(10))
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "-5.1em")
+            .attr("text-anchor", "end")
+            .attr("stroke", "black")
+            .text("Amount");
 
-g.selectAll(".bar")
-    .data(data)
-    .enter().append("rect")
-    .attr("class", "bar")
-    .attr("x", function(d) { return xScale(d.year); })
-    .attr("y", function(d) { return yScale(d.amount); })
-    .attr("width", xScale.bandwidth())
-    .attr("height", function(d) { return height - yScale(d.amount); })
-    .attr("fill", "#0d4aa5");
+        g.selectAll(".bar")
+            .data(fetchedData)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function (d) { return xScale(d.userId); })
+            .attr("y", function (d) { return yScale(d.id); })
+            .attr("width", xScale.bandwidth())
+            .attr("height", function (d) { return height - yScale(d.id); })
+            .attr("fill", "#0d4aa5");
+    })
+
